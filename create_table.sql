f719b4bedf8b4616cf4816066f0ec553f86fdf06
ercas <edgarrimandocastro@gmail.com>
2018-04-12 16:20:35 -0400

    added places.bounding_box, tweets.media, tweets.quoted_status_id, changed tweets.id from varchar to bigint

--- create_table.sql
+++ create_table.sql
@@ -23,16 +23,19 @@ CREATE TABLE places(
 	full_name VARCHAR,
 	place_type VARCHAR
 );
+ALTER TABLE places ADD COLUMN bounding_box geometry(Polygon, 4326);
 
 CREATE TABLE tweets(
-	id VARCHAR PRIMARY KEY,
+	id BIGINT PRIMARY KEY,
 	user_id BIGINT REFERENCES users(id),
 	place_id VARCHAR REFERENCES places(id),
 	text VARCHAR,
 	created_at TIMESTAMP,
 	hashtags VARCHAR[],
 	urls VARCHAR[],
+	media VARCHAR[],
 	lang VARCHAR,
+	quoted_status_id BIGINT,
 	mentioned_user_ids BIGINT[],
 	in_reply_to_status_id VARCHAR,
 	in_reply_to_user_id BIGINT
--- import_tweets.py
+++ import_tweets.py
@@ -4,6 +4,7 @@ import psycopg2
 import shapely.geometry
 
 NLONG = "$numberLong"
+DEFAULT_COMMIT_INTERVAL = 5000
 
 def getitem_or_none(dict_, key):
     """ Attempt to get an item from a dict; if it does not exist, return None
@@ -22,11 +23,22 @@ def convert_nlong(nlong):
 
 class Importer(object):
 
-    def __init__(self):
+    def __init__(self, commit_interval = DEFAULT_COMMIT_INTERVAL):
+        """ Initialize Importer object
+
+        Args:
+            commit_interval: The number of tweets that a commit should be
+                automatically made after
+        """
+
         self.connection = psycopg2.connect("dbname = geotweets")
         self.cursor = self.connection.cursor()
+        self.n_imported = 0
+        self.commit_interval = commit_interval
 
     def import_tweet(self, data):
+        """ Import a tweet """
+
         user = data["user"]
         place = data["place"]
         user_id  = convert_nlong(user["id"])
@@ -59,15 +71,16 @@ class Importer(object):
             place_id = place["id"]
             self.cursor.execute(
                 """INSERT INTO places
-                    (id, country, full_name, place_type)
+                    (id, country, full_name, place_type, bounding_box)
                 VALUES
-                    (%s, %s, %s, %s)
+                    (%s, %s, %s, %s, ST_SetSRID(%s::geometry, 4326))
                 ON CONFLICT DO NOTHING""",
                 (
                     place["id"],
                     place["country"],
                     place["full_name"],
-                    place["place_type"]
+                    place["place_type"],
+                    shapely.geometry.shape(place["bounding_box"]).wkb_hex
                 )
             )
         else:
@@ -81,6 +94,14 @@ class Importer(object):
             ]
         else:
             urls = None
+
+        if ("media" in entities):
+            media = [
+                media["media_url"]
+                for media in entities["media"]
+            ]
+        else:
+            media = None
 
         if ("hashtags" in entities):
             hashtags = [
@@ -101,10 +122,10 @@ class Importer(object):
         self.cursor.execute(
             """INSERT INTO tweets
                 (id, user_id, place_id, text, created_at, hashtags, urls,
-                lang, mentioned_user_ids, in_reply_to_status_id,
-                in_reply_to_user_id, coordinates)
+                media, lang, mentioned_user_ids, quoted_status_id,
+                in_reply_to_status_id, in_reply_to_user_id, coordinates)
             VALUES
-                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
                 ST_SetSRID(%s::geometry, 4326)
                 )
             ON CONFLICT DO NOTHING""",
@@ -116,13 +137,19 @@ class Importer(object):
                 data["created_at"],
                 hashtags,
                 urls,
+                media,
                 data["lang"],
                 mentioned_user_ids,
+                convert_nlong(getitem_or_none(data, "quoted_status_id")),
                 convert_nlong(getitem_or_none(data, "in_reply_to_status_id")),
                 convert_nlong(getitem_or_none(data, "in_reply_to_user_id")),
                 shapely.geometry.shape(data["coordinates"]).wkb_hex
             )
         )
+
+        self.n_imported += 1
+        if (self.n_imported % self.commit_interval):
+            self.connection.commit()
 
 if (__name__ == "__main__"):
     import json
@@ -131,15 +158,9 @@ if (__name__ == "__main__"):
     importer = Importer()
 
     with open(sys.argv[1], "r") as f:
-        i = 0
         for line in f:
             importer.import_tweet(json.loads(line))
-            i += 1
-
-            if (i % 5000):
-                importer.connection.commit()
-
-            sys.stdout.write("\rimported %d tweets" % i)
+            sys.stdout.write("\rimported %d tweets" % importer.n_imported)
             sys.stdout.flush()
 
     print("")
